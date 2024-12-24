737f85bafc658308ea377a3277e5a2d04f941802
Eser DENIZ <srwiez@gmail.com>
2024-12-24 22:31:32 +0100

    Final API

--- src/System.php
+++ src/System.php
@@ -2,18 +2,22 @@
 
 namespace KnotsPHP\System;
 
-use KnotsPHP\System\Contracts\OperatingSystemContract as OperatingSystemContract;
+use KnotsPHP\System\Contracts\OperatingSystemContract;
 use KnotsPHP\System\Enums\OperatingSystem;
+use KnotsPHP\System\OperatingSystems\FreeBSD;
+use KnotsPHP\System\OperatingSystems\Linux;
+use KnotsPHP\System\OperatingSystems\MacOS;
+use KnotsPHP\System\OperatingSystems\Windows;
 
 class System
 {
     public static function os(): OperatingSystemContract
     {
         return match (OperatingSystem::current()) {
-            OperatingSystem::Windows => new OperatingSystems\Windows,
-            OperatingSystem::MacOS => new OperatingSystems\MacOS,
-            OperatingSystem::Linux => new OperatingSystems\Linux,
-            OperatingSystem::FreeBSD => new OperatingSystems\FreeBSD,
+            OperatingSystem::Windows => new Windows,
+            OperatingSystem::MacOS => new MacOS,
+            OperatingSystem::Linux => new Linux,
+            OperatingSystem::FreeBSD => new FreeBSD,
         };
     }
 }
